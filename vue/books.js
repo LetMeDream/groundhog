80922c7bd9740ed2ee3e4abd510a57a58cfaaafc
LetMeDream <raulalfonzo66@gmail.com>
2020-12-07 04:49:13 -0400

    Iniciando cambios en Vue

---
+++ vue/books.js
@@ -0,0 +1,57 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      books: [
+          {
+                id: 1,
+                title: 'Python Crash Course with Vue',
+                price: '$6.69',
+                imgDescription: 'Python Book',
+                length: '270 pages',
+                description: 'A Hands-On, Project-Based Introduction to Programming',
+                imgSrc: "img/js.jpg",
+                detailsList: []
+          },
+          {
+            id:2,
+            title: 'Eloquent Javascript',
+            price: '$18.00',
+            imgDescription: 'Javascript Book',
+            length: 'Unnknown',
+            description: "Completely revised and updated, this best-selling introduction to programming in JavaScript focuses on writing real applications.",
+            imgSrc: "img/py.jpg",
+            detailsList: []
+          },
+          {
+            id:3,
+            title: 'CSS, The Definitive Guide',
+            price: '$48.88',
+            imgDescription: 'CSS',
+            length: 'Unnknown',
+            description: "If you’re a web designer or app developer interested in sophisticated page styling, improved accessibility, and saving time and effort, this book is for you.",
+            imgSrc: "img/css.jpg",
+            detailsList: []
+          },
+          {
+            id:4,
+            title: 'Creating Interfaces with Bulma',
+            price: '$ 48.88',
+            imgDescription: 'Bulma Book',
+            length: 'Unnknown',
+            imgSrc: "img/bulma.jpg",
+            detailsList: ['• Create layouts with Bulma','• Work with components in Bulma','• Design specific elements for your UI', '• Extend components with your own setup', 'Learn to integrati Bulma with JavaScript through the following frameworks: React, Angular, VueJS, and Vanilla JS.']
+          },
+          {
+            id:5,
+            title: 'The Analyst',
+            price: '$2.50 (Used and new)',
+            imgDescription: 'The analyst Book',
+            description:'El thriller psicológico es la novela más exitosa del autor y en 2003 se publicó en español en España, Chile, Argentina, Colombia, Venezuela, México, Uruguay y Ecuador, donde se mantuvo entre los más vendidos.',
+            length: '376',
+            imgSrc: "img/analyst.jpg",
+            detailsList:[]
+          }
+      ]
+    }
+});
